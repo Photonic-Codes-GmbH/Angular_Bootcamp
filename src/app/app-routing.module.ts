38f53dd89597b52696576ca951a1d1cc0b107229
Christian Wiegand <christianwiegand1987@yahoo.de>
2022-09-09 17:06:07 +0200

    added routes to both page buttons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstpageComponent } from './firstpage/firstpage.component';
+import { SecondpageComponent } from './secondpage/secondpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FirstpageComponent },
+  { path: 'firstpage', component: FirstpageComponent },
+  { path: 'secondpage', component: SecondpageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SecondpageComponent } from './secondpage/secondpage.component';
+import { FirstpageComponent } from './firstpage/firstpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SecondpageComponent,
+    FirstpageComponent
   ],
   imports: [
     BrowserModule,
