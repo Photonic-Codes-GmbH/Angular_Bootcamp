f52b40156cee6d402a6d224644b67d8f8459d1ce
Christian Wiegand <christian.wiegand@photonic-codes.com>
2022-09-10 23:44:55 +0200

    You can now enter data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,16 @@ export class AppComponent {
   title = 'My Cool App';
   myName = "Christian";
   itemImageUrl = '../assets/Logo_schwarze Schrift_ohne Hintergrund.png';
+  number = 0
+  numberPlus5 = 5
+  times2 = 10
+
+  calculate(numberInput: number){
+
+    this.number = numberInput
+    this.numberPlus5 = this.number + 5
+    this.times2 = this.numberPlus5 * 2
+  }
 
   changeData(){
 
