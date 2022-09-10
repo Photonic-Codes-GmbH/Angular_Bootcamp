3d95340993c99a5bf6e726d5a5a9ba54d779b7c4
Christian Wiegand <christian.wiegand@photonic-codes.com>
2022-09-10 20:08:54 +0200

    Data is shown with interpolation and property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular_Bootcamp';
+  title = 'My Cool App';
+  myName = "Christian";
+  itemImageUrl = '../assets/Logo_schwarze Schrift_ohne Hintergrund.png';
 }
