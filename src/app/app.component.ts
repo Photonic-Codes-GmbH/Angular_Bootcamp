ff9f35c92569f7c1615b6154260d44ae1bd2cacb
heavygand <35569049+heavygand@users.noreply.github.com>
2022-09-23 16:52:02 +0200

    changes for stackblitz #2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'My Cool App';
   myName = "Christian";
-  itemImageUrl = 'https://raw.githubusercontent.com/heavygand/Angular_Bootcamp/enter_data/src/assets/Logo_schwarze%20Schrift_ohne%20Hintergrund.png?token=GHSAT0AAAAAABZBWMH4J7CTCHCM3XFWT2LAYZNX7UA';
+  itemImageUrl = 'https://github.com/heavygand/Angular_Bootcamp/raw/4b52b4c30f81795e8908a930f4d2ebb5368c3f4b/src/assets/Logo_schwarze%20Schrift_ohne%20Hintergrund.png';
   number = 0
   numberPlus5 = 5
   times2 = 10
