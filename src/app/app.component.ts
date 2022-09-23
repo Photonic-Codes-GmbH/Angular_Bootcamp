ae3308a523b68cd57591a27d7cee586b9772d127
heavygand <35569049+heavygand@users.noreply.github.com>
2022-09-23 22:24:01 +0200

    App title toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'My Cool App';
-  myName = "Christian";
-  itemImageUrl = 'https://github.com/heavygand/Angular_Bootcamp/raw/4b52b4c30f81795e8908a930f4d2ebb5368c3f4b/src/assets/Logo_schwarze%20Schrift_ohne%20Hintergrund.png';
+  myName = 'Christian';
+  itemImageUrl =
+    'https://github.com/heavygand/Angular_Bootcamp/raw/4b52b4c30f81795e8908a930f4d2ebb5368c3f4b/src/assets/Logo_schwarze%20Schrift_ohne%20Hintergrund.png';
 
-  changeData(){
+  changeData() {
 
-    this.title = "Something nice"
+    this.title == 'Something nice' ? this.title = 'My Cool App' : this.title = 'Something nice'
   }
 }
